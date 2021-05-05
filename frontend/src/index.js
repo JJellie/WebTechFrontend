a50a79da4f77f497f082060329e06fe7be37d58f
JJellie <Jelmerlap@gmail.com>
2021-05-05 16:58:21 +0200

    Web vitals removed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Css/index.css';
 import About from './Pages/About';
-import reportWebVitals from './reportWebVitals';
 import HomePage from './Pages/Homepage';
 import Vis from './Pages/vis';
 import {
@@ -83,4 +82,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
