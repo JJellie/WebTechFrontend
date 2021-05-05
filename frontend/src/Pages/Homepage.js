e52175b1b09160c4463332b029ab36cd8a0118d8
Lieke-TUe <83067077+Lieke-TUe@users.noreply.github.com>
2021-05-05 21:25:51 +0200

    CSS (attempt 2 ;-;)

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -5,12 +5,17 @@ import "../Css/Homepage.css"
 class HomePage extends React.Component {
     render () {
         return (
-            <div> 
+            <div>
+            <div class="first"> 
                 <h1> Welcome to the homepage of the Amazing DBL team! </h1>
                 <p>This page is still in testing phase. If you have any remarks, please do let us know.</p>
-
-                <img src="https://i.imgur.com/OXhppnI.png" alt = "Banner picure" height="400" width = "1300"></img>
             </div>
+
+            <div class="second">
+                <img src="https://i.imgur.com/OXhppnI.png" alt = "Banner picure" ></img>
+            </div>
+            </div>
+
             );
         
     }
