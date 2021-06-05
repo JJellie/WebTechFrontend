789ef7f7d0c0b779e7dcf9cc896946c30bd4b8b2
Lieke-TUe <83067077+Lieke-TUe@users.noreply.github.com>
2021-06-05 15:26:52 +0200

    Added a few comments

--- frontend/src/Pages/vis.js
+++ frontend/src/Pages/vis.js
@@ -79,11 +79,13 @@ class Vis extends React.Component {
                     <label className='testSetText' id='testSet' onClick={() => this.loadTestSet()}>Or click here to use a test data set</label>
                 </div>
             </div>
-            <div> 
+            <div>
+                {/* ~~~~  This is the main title ~~~~  */} 
                 <div className="visHeader">
                     <h1>Visualisation</h1>
                 </div>
 
+                {/* ~~~~  This is the top divider  ~~~~  */}
                 <div className="line">
                     {/* this will be a line */}
                 </div>
@@ -92,14 +94,15 @@ class Vis extends React.Component {
                     {/* this will be a line */}
                 </div>
 
+                {/* ~~~~  This is the text displayed between dividers  ~~~~  */}
                 <div className="visText">
                      Wecome to the visualisation page. You can upload your data using the big blue button below. In the case you do not have a dataset, you can also use the standard dataset.
                         <br></br>
                         When your data is uploaded, the visualisations will be loaded in. For some extra tips, refer to our <Link to="../about">About page</Link>. <br></br>
-                        When the visualisations have loaded, you can select our visual-impairment friendly modes. You can choose a colour scheme, or go for the extra experience with audio cues upon hovering.
-                        
+                        When the visualisations have loaded, you can select our visual-impairment friendly modes. You can choose a colour scheme, or go for the extra experience with audio cues upon hovering.                        
                 </div>
 
+                {/* ~~~~  This is the bottom divider  ~~~~  */}
                 <div className="line">
                     {/* this will be a line */}
                 </div>
@@ -108,6 +111,7 @@ class Vis extends React.Component {
                     {/* this will be a line */}
                 </div>
 
+                {/* ~~~~  This is the upload button which opens the popup ~~~~  */}
                 <div className="uploadbutton">
                     <button id='openPopup' onClick={() => this.popupShow()}>Upload your data here!</button>
                 </div>
