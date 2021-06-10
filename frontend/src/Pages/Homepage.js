5af94a8c5fd2f7189fa99ea01070c2b5545a00c8
Lieke-TUe <83067077+Lieke-TUe@users.noreply.github.com>
2021-06-10 17:28:13 +0200

    Update Homepage

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import "../Css/Homepage.css";
 import Banner from "../Images/DBL_Banner.png";
+import BannerNew from "../Images/DBL_banner_new.png";
+import Divider from "../Images/divider.png";
 import {
     Link,
   } from "react-router-dom";
@@ -10,24 +12,28 @@ class HomePage extends React.Component {
         this.props.update()
     }
 
-
     render () {
         return (
-            <div>
-                <div className="first"> 
-                    <h1>NAME</h1>
-                    <h2>By the Amazing DBL team</h2>
-                    <p> Basic visualization tool for visualizing <br></br> Nice Adjacency Matrices and Thread Arcs <br></br> Start making your visualizations now for free </p>
+            <div className = "Total">
+                <div className = "Banner">
+                    {/* <img src= {BannerNew} alt = "Oops! This image was loaded incorrectly" ></img> */}
+                    <div className = "BannerText">
+                        <h1> Graphify </h1>
+                        <h2>By the Amazing DBL team</h2>
+                    </div>
+                    <div className = "BannerButton">
                     <Link to="/vis"><button>Start visualizing</button></Link>
+                    </div>
                 </div>
 
-                <div className="second">
-                    <img src={Banner} alt = "Banner picure" ></img>
+                <div className = "BannerBottomText">
+                    <p> basic visualization tool for visualizing    ◈   nice Adjacency Matrices and Thread Arcs <br></br> Start making your visualizations now for free! </p>
                 </div>
+
+
+
             </div>
-
-            );
-        
+        )
     }
 }
 
