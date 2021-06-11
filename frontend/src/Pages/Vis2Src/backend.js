ad152c93b9cd95e9a53b2f85d297ab81b91f404e
Ching Chou <chingchou34@gmail.com>
2021-06-11 12:40:45 +0200

    bla bla

--- frontend/src/Pages/Vis2Src/backend.js
+++ frontend/src/Pages/Vis2Src/backend.js
@@ -34,10 +34,10 @@ async function postDataSet(file = null) {
 async function recieveColumns(filename) {
     let cols = await fetch('http://localhost:3001/download/columns?file='+filename, {method: "GET"});
     let val =  await cols.json();
-    if (val['error']) {
-        throw new Error(val['error']);
-    }
-    return val['columns'];
+    // if (val['error']) {
+    //     throw new Error(val['error']);
+    // }
+    return val;
 }
 
 //Function for sending the sorted columns to the backend for full dataset processing
@@ -52,9 +52,12 @@ export function DatasetPopup({ setDataSet }) {
     const [filename, setFilename] = useState(null);
     const [popupState, setPopupState] = useState(initialPopupState);
     const [columnNames, setColumns] = useState(null);
+    const [menuCount, setMenuCount] = useState(0);
+
     function dataPassing(filename) {
         setDataSet(getDataset(filename))
     }
+    
     function ShowSubmit() {
         try {
             setFilename(document.getElementById('UploadedFile').files[0].name);
@@ -63,35 +66,65 @@ export function DatasetPopup({ setDataSet }) {
             //TODO
         }
     }
-    
-    return (
-        <div id='popup' className='popup'>
-            <div className='popup-content'>
-                {popupState === "fileSubmit" || popupState === "loading" ?
+
+    function popupPage(popupState, menuCount) {
+        switch(popupState) {
+            case "fileSubmit":
+            case "loading":
+                return (
                     <>
-                        <span onClick={() => {closePopup(); setPopupState(initialPopupState)}} className="close">&times;</span>
+                    
                         <label htmlFor='UploadedFile' className='UploadButton'>Choose file here</label> 
                         <input type = "file" accept = ".csv"  id="UploadedFile" onChange={()=> ShowSubmit()}></input>
                         <span className='nameDisplay'>Chosen file's name:<span className='fileName'>{filename}</span></span>
                         {popupState === "fileSubmit" ?
                             <>
                                 <label htmlFor='Submit' id='SubmitBut' className='SubmitButton'><span>Confirm</span></label> 
-                                <input type = "submit" id='Submit' onClick={async () => {setPopupState("loading"); setColumns(await postDataSet(document.getElementById('UploadedFile').files[0])); setPopupState("columnSorting")}} className="UploadButton"></input>
+                                <input 
+                                    type = "submit" 
+                                    id='Submit' 
+                                    onClick={async () => {
+                                        setPopupState("loading"); 
+                                        setColumns(await postDataSet(document.getElementById('UploadedFile').files[0])); 
+                                        setPopupState("columnSorting")}} 
+                                    className="UploadButton"></input>
                             </>
                             :
                                 <label htmlFor='Submit' id='SubmitBut' className='SubmitButton SubmitButtonLoading'><span></span></label> 
                         }
                         <label className='testSetText' id='testSet' onClick={() => {}}>Or click here to use a test data set</label>
                     </>
-                :
-                    <>
-                        <span onClick={() => {(closePopup()); setPopupState(initialPopupState)}} className="close">&times;</span>
-                        <div>Lorum ipsum dolor sit amet</div>
-                        {columnNames.map((columnName) => (
-                            <div className='dataCols' key={columnName}>{columnName}</div>
-                        ))}
-                    </>
+                )
+            case "columnSorting":
+                switch(menuCount) {
+                    case 0:
+                        console.log(columnNames);
+                        return (
+                            <div className="ColumnSortingMenu">
+                                <p>To fully be able to use your uploaded dataset, we need you to identify which columns correspond to which attributes of the dataset. In the next few pages you can fill in what column of your dataset corresponds to the prompted attribute. </p>
+                                
+                                <button onClick={() => {setMenuCount(menuCount + 1)}}>Next</button>
+                                <button onClick={() => {setMenuCount(menuCount - 1)}}>Back</button>
+                            </div>
+                        )
+                    case 1:
+                        return (
+                            <div>hello</div>
+                        )
+                    case 2:
+                        return (
+                            <div>hello</div>
+                        )
                 }
+        }
+    }
+
+    
+    return (
+        <div id='popup' className='popup'>
+            <div className='popup-content'>
+                <span onClick={() => {closePopup(); setPopupState(initialPopupState); setMenuCount(0)}} className="close">&times;</span> 
+                {popupPage(popupState, menuCount)} 
             </div>
         </div>
     );
