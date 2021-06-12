2e572c04c923155407308dd8cc6fc75ee36e5aaa
Lieke-TUe <83067077+Lieke-TUe@users.noreply.github.com>
2021-06-12 23:55:09 +0200

    Homepage Added Stuff
    Disfunctional images tho ;-;;;;;

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -3,6 +3,7 @@ import "../Css/Homepage.css";
 import Banner from "../Images/DBL_Banner.png";
 import BannerNew from "../Images/DBL_banner_new.png";
 import Divider from "../Images/divider.png";
+import Logo from '../Images/logo 1.png';
 import {
     Link,
   } from "react-router-dom";
@@ -18,19 +19,70 @@ class HomePage extends React.Component {
                 <div className = "Banner">
                     {/* <img src= {BannerNew} alt = "Oops! This image was loaded incorrectly" ></img> */}
                     <div className = "BannerText">
-                        <h1> Graphify </h1>
+                        <h1> Graphify™ </h1>
                         <h2>By the Amazing DBL team</h2>
                     </div>
                     <div className = "BannerButton">
-                    <Link to="/vis"><button>Start visualizing</button></Link>
+                        <Link to="/vis"><button>Start visualizing</button></Link>
+                        <p> Scroll down for more information... </p>
                     </div>
                 </div>
 
-                <div className = "BannerBottomText">
-                    <p> basic visualization tool for visualizing    ◈   nice Adjacency Matrices and Thread Arcs <br></br> Start making your visualizations now for free! </p>
+                <div class="marquee">
+                    <div>
+                        <span> ◈    basic visualization tool for visualizing    ◈   nice Adjacency Matrices and Thread Arcs ◈   Start making your visualizations now for free! </span> 
+                        <span> ◈    basic visualization tool for visualizing    ◈   nice Adjacency Matrices and Thread Arcs ◈   Start making your visualizations now for free! </span>
+                    </div>
                 </div>
 
+                <div className = "GeneralAd">
+                    <div className="imageHolder">
+                        {/* <img src={Logo} alt="company logo"></img>  */}
+                    </div>
+                    <div className = "generalText">
+                        <p>
+                        On this website you can find 2 beautiful visualisations, carefully programmed by the Amazing Dbl Team. They have put in a grand total of 840 hours, spread over 6 members.
+                        Below you will be able to find screenshots of each of the beautiful visualisations, some general information, and a short description of what each page will contain. 
+                        Thank you for making use of our accessible visualisation tool, Graphify™, by the Amazing DBL Team.
+                        </p>
+                    </div>
+                </div>
 
+                <div className = "ThreadArcsAd">
+                    <img src="https://treevis.net/thumbs200png/Kerr2003.png" alt="company logo"></img>
+                    <div className = "ThreadText">
+                        <p>
+                        We will visualise your network data into a "ThreadArc" visualisation. 
+                        This visualisation displays the nodes as circles and edges as curved lines. 
+                        For more information on how to interact with this visualisation and how to read it, see our <Link to="../about">About page</Link>.
+                        </p>
+                    </div>
+                </div>
+
+                <div className = "AdjacencyAd">
+                    <img src="https://i.imgur.com/Ba7GQYL.png" alt="company logo"></img>
+                    <div className = "AdjacencyText">
+                        <p>
+                        We will visualise your network data into a "Adjacency Matrix" visualisation. 
+                        This visualisation displays the nodes as headers and edges as coloured cells. 
+                        For more information on how to interact with this visualisation and how to read it, see our <Link to="../about">About page</Link>.
+                        </p>
+                    </div>
+
+                </div>
+
+                <div className = "PageAd">
+                    <img src="https://64.media.tumblr.com/f7f2ff25655c40deb3314e87e5a70fc1/tumblr_pp4p31ZgrF1t4tgk0_1280.png" alt="froge"></img>
+                    <div className = "PageText">
+                        <p>
+                        Our website has 4 pages. 
+                        Firstly the home page. This is where you are now.
+                        Secondly the visualisation page. This page contains the visualisations. Upload your data here to get started. You can navigate to this page by clicking at the top, or by clicking <Link to="../vis">here</Link>. <br></br>
+                        Thirdly the about page. This page provides an extra explanation for the visualisations, as well as a usage. You can navigate to this page by clicking at the top, or by clicking <Link to="../about">here</Link>. <br></br>
+                        Lastly the contact page. This provides our contact details for any other projects, and some personal information. You can navigate to this page by clicking at the top, or by clicking <Link to="../contact">here</Link>.
+                        </p>
+                    </div>
+                </div>
 
             </div>
         )
